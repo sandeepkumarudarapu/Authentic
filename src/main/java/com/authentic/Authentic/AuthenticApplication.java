76bfd31134c82b176d0aad224f3057b7e686c5d9
Sandeep Kumar Udarapu <sandeep.kumar@vrize.com>
2022-01-11 08:26:27 +0530

    master

---
+++ src/main/java/com/authentic/Authentic/AuthenticApplication.java
@@ -0,0 +1,13 @@
+package com.authentic.Authentic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/authentic/Authentic/AuthenticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authentic.Authentic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
